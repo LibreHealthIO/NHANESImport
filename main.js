1854c3da8e1bf9cd181c80f4fb2edf405cee3ae6
Kevin Yeh <kevinyeh@alum.mit.edu>
2017-03-14 02:29:53 -0400

    Loading CSV

--- main.js
+++ main.js
@@ -9,12 +9,14 @@ var path = require('path');
 var request = require('request');
 var fs = require('fs');
 var mysql=require('mysql');
+var csv=require('csv-parse/lib/sync');
 
 var dbConn = mysql.createConnection({
         host: 'localhost',
         user : 'root',
         password : 'mydbpwd',
-        database : 'nhanes'
+        database : 'nhanes',
+        multipleStatements: true
     });
 
 dbConn.connect();
@@ -206,7 +208,7 @@ function createConvertScript(year)
 
     dbConn.query("SELECT * FROM filelist WHERE year=?",[year],function(err,rows)
     {
-        var script="import(Nmisc)\n";
+        var script="library(Nmisc)\n";
         for(var fileIdx=0;fileIdx<rows.length;fileIdx++)
         {
             var curRow=rows[fileIdx];
@@ -220,10 +222,97 @@ function createConvertScript(year)
             script+="write.csv("+baseFile+", file = \""+outputFile + "\")\n";
             
         }
-        console.log(script);
+        fs.writeFileSync("createCSV"+year+".R",script);
+    });
+
+}
+
+function dropAndCreateTable(tablename,records)
+{
+    var columns=[];
+    var columnsCreate= "";
+    var columnsCount=0;
+    var insertColumnDefs=" (";
+    var colParams = " (";
+    for(var column in records[0])
+    {
+        columns.push(column);
+        if(column==="")
+        {
+            columnsCreate+="id int primary key"
+            insertColumnDefs+="id";
+            colParams+="?";
+        }
+        else if(column==="seqn")
+        {
+            columnsCreate+=",\nseqn int"
+            insertColumnDefs+=", seqn";
+            colParams+=", ?";
+        }
+        else
+        {
+            columnsCreate+= ",\n";
+            columnsCreate+= column + " " + "double";
+            insertColumnDefs+=", "+column;
+            colParams+=", ?";
+        }
+        columnsCount++;
+    }
+    insertColumnDefs+=")";
+    colParams+=")";
+    columnsCreate +=",\n INDEX `seqn_idx` (`seqn`)"
+    var dropSQL = " DROP TABLE IF EXISTS " + tablename + ";"
+    var createSQL = " CREATE TABLE " + tablename + "(";
+    createSQL+=columnsCreate + ");";
+    var insertSQL = " INSERT INTO " + tablename + insertColumnDefs + " VALUES "+colParams;
+    dbConn.query(dropSQL + createSQL,function(err,result){
+        if(err)
+        {
+            console.log(dropSQL+createSQL);
+            console.log(err);
+        }
+        for(var recordIdx=0;recordIdx<records.length;recordIdx++)
+        {
+            var parameters=[];
+            var curRecord=records[recordIdx];
+            console.log(JSON.stringify(curRecord));
+            for(var colIdx=0;colIdx<columns.length;colIdx++)
+            {
+                var curValue=curRecord[columns[colIdx]];
+                if(curValue==='NA')
+                {
+                    parameters.push(null);                    
+                }
+                else
+                {
+                    parameters.push(curValue);
+                }
+            }
+            dbConn.query(insertSQL,parameters,function(err2,result2)
+            {
+                if(err2)
+                {
+                    console.log(insertSQL);
+                    console.log(err2);
+                }
+            });
+
+            //        console.log(curRecord[columns[1]]);
+
+        }
+        
     });
     
+}
 
+function loadCSVfile(year,filename)
+{
+    var baseFile=filename.substring(0,filename.lastIndexOf("."));
+    var yearPath=process.cwd() + path.sep + "datafiles"+ path.sep + year+ path.sep;
+    var filedata=fs.readFileSync(yearPath+filename,"utf-8");
+    var records=csv(filedata,{columns: true});
+    
+    dropAndCreateTable(baseFile,records);
 }
 
 function process_data()
@@ -233,4 +322,4 @@ function process_data()
     createConvertScript(2011);
 }
 
-process_data();
+loadCSVfile(2011,"DEMO_G.csv");
